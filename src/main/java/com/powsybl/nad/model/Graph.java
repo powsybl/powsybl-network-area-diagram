9f28dc3f5acdceba4782bc3cd37dbe6b1d8dd1fb
Florian Dupuy <florian.dupuy@rte-france.com>
2021-10-08 16:40:03 +0200

    Add model classes
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

---
+++ src/main/java/com/powsybl/nad/model/Graph.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.nad.model;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * @author Florian Dupuy <florian.dupuy at rte-france.com>
+ */
+public class Graph {
+
+    private final Map<String, Node> nodes = new LinkedHashMap<>();
+    private final Map<String, Edge> edges = new LinkedHashMap<>();
+
+    public void addNode(Node node) {
+        nodes.put(node.getDiagramId(), node);
+    }
+
+    public void addEdge(Edge edge) {
+        edges.put(edge.getDiagramId(), edge);
+    }
+
+    public Stream<Node> getNodesStream() {
+        return nodes.values().stream();
+    }
+
+    public Stream<Edge> getEdgesStream() {
+        return edges.values().stream();
+    }
+
+    public Optional<Node> getNode(String diagramId) {
+        return Optional.ofNullable(nodes.get(diagramId));
+    }
+
+    public Optional<Edge> getEdge(String diagramId) {
+        return Optional.ofNullable(edges.get(diagramId));
+    }
+
+}
---
+++ src/main/java/com/powsybl/nad/model/TextNode.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.nad.model;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Florian Dupuy <florian.dupuy at rte-france.com>
+ */
+public class TextNode extends AbstractNode {
+
+    private final String text;
+    private TextEdge edge;
+
+    protected TextNode(String diagramId, String text) {
+        super(diagramId);
+        this.text = text;
+    }
+
+    @Override
+    public List<TextEdge> getAdjacentEdges() {
+        return Collections.singletonList(edge);
+    }
+
+    public void setEdge(TextEdge edge) {
+        this.edge =  edge;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
