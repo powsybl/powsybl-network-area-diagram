1bad8778b5638a8de525630abee696c3018db5d7
Florian Dupuy <florian.dupuy@rte-france.com>
2022-03-02 14:15:19 +0100

    Add unit test
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- src/test/java/com/powsybl/nad/svg/EdgeInfoLabelTest.java
+++ src/test/java/com/powsybl/nad/svg/EdgeInfoLabelTest.java
@@ -25,7 +25,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * @author Florian Dupuy <florian.dupuy at rte-france.com>
  */
-class EdgeInfoTest extends AbstractTest {
+class EdgeInfoLabelTest extends AbstractTest {
 
     private String internalLabel;
     private String externalLabel;
