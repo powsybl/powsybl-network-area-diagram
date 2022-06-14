1d2c85c396b734a76e19917373be87c4a00ac4e0
Florian Dupuy <florian.dupuy@rte-france.com>
2022-06-14 14:24:15 +0200

    fix
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- src/main/java/com/powsybl/forcelayout/ForceLayout.java
+++ src/main/java/com/powsybl/forcelayout/ForceLayout.java
@@ -38,7 +38,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
--- src/main/java/com/powsybl/nad/build/iidm/NetworkGraphBuilder.java
+++ src/main/java/com/powsybl/nad/build/iidm/NetworkGraphBuilder.java
@@ -8,15 +8,13 @@ package com.powsybl.nad.build.iidm;
 
 import com.powsybl.commons.PowsyblException;
 import com.powsybl.iidm.network.Identifiable;
-import com.powsybl.nad.model.ThreeWtNode;
 import com.powsybl.iidm.network.*;
 import com.powsybl.nad.build.GraphBuilder;
 import com.powsybl.nad.model.*;
 import com.powsybl.nad.utils.iidm.IidmUtils;
 
-import java.util.*;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * @author Florian Dupuy <florian.dupuy at rte-france.com>
