f2f3ee077a8fbf5ac0438445845753c3ff1e0f0b
Luma <zamarrenolm@aia.es>
2022-11-03 16:39:05 +0100

    unit test for fixed layout
    Signed-off-by: Luma <zamarrenolm@aia.es>

--- src/main/java/com/powsybl/nad/model/Point.java
+++ src/main/java/com/powsybl/nad/model/Point.java
@@ -25,6 +25,23 @@ public class Point {
         this(0, 0);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
     public static Point createMiddlePoint(Point point1, Point point2) {
         Objects.requireNonNull(point1);
         Objects.requireNonNull(point2);
---
+++ src/test/java/com/powsybl/nad/layout/FixedLayoutTest.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.nad.layout;
+
+import com.powsybl.iidm.network.Network;
+import com.powsybl.nad.build.iidm.NetworkGraphBuilder;
+import com.powsybl.nad.build.iidm.VoltageLevelFilter;
+import com.powsybl.nad.model.Graph;
+import com.powsybl.nad.model.Point;
+import com.powsybl.nad.svg.NetworkTestFactory;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Luma Zamarreno <zamarrenolm at aia.es>
+ */
+class FixedLayoutTest {
+
+    @Test
+    void testCurrentLimits() {
+        Network network = NetworkTestFactory.createTwoVoltageLevels();
+
+        Map<String, Point> expected = Map.of(
+                "vl1", new Point(1, 0),
+                "vl2", new Point(2, 1));
+        Graph graph = new NetworkGraphBuilder(network, VoltageLevelFilter.NO_FILTER).buildGraph();
+        Layout fixedLayout = new BasicFixedLayoutFactory().create();
+        fixedLayout.setInitialNodePositions(expected);
+        Map<String, Point> actual = fixedLayout.run(graph, new LayoutParameters());
+        assertEquals(expected, actual);
+    }
+}
